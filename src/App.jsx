1619c71dfecb1b30e021105553ddc7819d33e33d
Sherif Hamdy <79402373+SHEFOO10@users.noreply.github.com>
2024-08-19 02:05:47 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Dashboard } from './Dashboard';
+
+
+function App() {
+
+  return (
+    <div className="h-screen w-screen flex justify-center items-center">
+        <Dashboard />
+    </div>
+
+  )
+}
+
+export default App
