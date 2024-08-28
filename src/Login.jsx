1c85420767401c3e9090dbb73fae99e5e1a6a823
Sherif Hamdy <79402373+SHEFOO10@users.noreply.github.com>
2024-08-28 02:33:50 +0000

    checkLoggedIn fixed

--- src/Login.jsx
+++ src/Login.jsx
@@ -14,6 +14,7 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { useEffect, useRef } from "react"
 import { useNavigate } from "react-router-dom"
+import { checkLoggedIn } from "./components/auth"
 
   
 export function Login() {
