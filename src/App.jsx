d500a559b41373c59df5ac2d6c0d13876d18db1d
Sherif Hamdy <79402373+SHEFOO10@users.noreply.github.com>
2024-08-28 00:22:10 +0000

    protected routes & handle logout

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,26 @@ import { Login } from './Login';
 import { Signup } from './Signup';
 import { Home } from './Home';
 import { Meet } from './Meet';
-
+import { AuthProvider } from './components/auth';
+import { RequireAuth } from './components/RequireAuth';
+import { Logout } from './components/Logout';
 
 
 function App() {
 
   return (
-    <Routes>
-    <Route path='/' element={<Home />} />
-    <Route path="/login" element={<Login />} />
-    <Route path="/signup" element={<Signup />} />
-    <Route path='/meet/:roomId' element={<Meet />} />
-    <Route path="*" element={<NotFound />} />
-  </Routes>
+    <AuthProvider>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path='/logout' element={<Logout />} />
+        <RequireAuth>
+          <Route path='/meet/:roomId' element={<Meet />} />
+        </RequireAuth>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </AuthProvider>
   )
 }
 
